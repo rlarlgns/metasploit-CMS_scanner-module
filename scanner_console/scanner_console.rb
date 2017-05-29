1d1cb0084ac50d568c79ce99c287b74e75219d36
rlarlgns <21kihoon@gmail.com>
2017-05-29 19:31:19 +0900

    update

--- scanner_console/scanner_console.rb
+++ scanner_console/scanner_console.rb
@@ -1,14 +1,13 @@
+#! /usr/bin/env ruby
 
-
-print "===============================\n"
+print "======================================\n"
 print "\t Scanner_MsfConsole \n"
-print "===============================\n"
+print "======================================\n"
 
-$:.unshift(File.join(File.dirname(__FILE__), '..' 'lib'))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'msf/base'
 require 'msfenv'
-require 'pry'
+require 'msf/base'
 
 Indent = '  '
 prompt = '> '
@@ -16,25 +15,33 @@ prompt = '> '
 class Console
 
     def initilize
+        @framework    = Msf::Simple::Framework.create
+        @exploit_name = nil
+        @payload_name = 'windows/meterpreter/reverse_tcp'
+        @input        = Rex::Ui::Text::Input::Stdio.new
+        @output       = Rex::Ui::Text::Output::Stdio.new
+        @driver       = Msf::ExploitDriver.new(@framework)
     end
-    
+
     def search_module(mod_name)
+        regx = Regexp.new(mode_name)
+        tbl = Rex::Ui::Text::Table.new()
     end
-    
+
     def set_option(opts)
     end
-    
+
     def show_options(to_see)
     end
-    
+
     def use_module(mod_name)
     end
-    
+
     def show_help
         tbl = Rex::Ui::Text::Table.new(
-            'Header'    => 'Commands you can use',
-            'Indent'    => Indent.length,
-            'Coulmns'   => [ 'Command', 'Description' ] 
+          'Header'    => 'Commands you can use',
+          'Indent'    => Indent.length,
+          'Columns'   => ['Command', 'Description']
         )
 
         tbl << ['help', 'list available commands']
@@ -43,7 +50,6 @@ class Console
         tbl.sort_rows(1)
         puts tbl.to_s
     end
-    
 end
 
 cs = Console.new
@@ -52,13 +58,11 @@ begin
     print prompt
     select = gets.chomp.split
 
-
     case select.shift
         when /help/
             cs.show_help
-
         when /search/
-
+            cs.search_module(select[0])
         when /use/
 
         when /show/
@@ -68,7 +72,6 @@ begin
         else
             print_error("unknown commands type 'help'")
      end
-
 rescue
-    print "error"
+    print "error\n"
 end while true
